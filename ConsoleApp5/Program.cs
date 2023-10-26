eb735fa10d05f1aab26c300e42809c12ebef78a3
afonaizer <123896092+afonaizer@users.noreply.github.com>
2023-10-26 15:59:52 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+
+
+        ////задан1
+        //static void Func()
+        //{
+        //    Console.Write("x= ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    int y = x * x;
+        //    Console.WriteLine($"y={y}");
+        //}
+        //static void Main(string [] args)
+        //{
+        //    Func();
+        //}
+
+        ////задан2
+        //static void Func()
+        //{
+        //    Console.Write("x= ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    int y = x * x;
+        //    Console.WriteLine($"y={y}");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Введите n= ");
+        //    int n = int.Parse(Console.ReadLine());
+        //    for (int i = 0; i <= n; i++)
+        //    { Func(); }
+        //    Console.ReadKey();
+        //}
+
+        ////задан2
+        //static void Func()
+        //{
+        //    Console.Write("x= ");
+        //    double x = int.Parse(Console.ReadLine());
+        //    double y = ((3*x)*(3*x) + 8*x-10)/15;
+        //    Console.WriteLine($"y={y}");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+        //задан3
+        //static void Func()
+        //{
+        //    Console.Write("a= ");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("b= ");
+        //    int b = int.Parse(Console.ReadLine());
+        //    Console.Write("h= ");
+        //    int h = int.Parse(Console.ReadLine());
+        //    int x = a;
+        //    int y;
+        //    for (int i = a; i <= b; i++)
+        //    {
+        //        y = x * x;
+        //        x = x + h;
+        //        Console.WriteLine($"y={y}");
+        //    }
+
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+
+        //задан4
+        //static void Func()
+        //{
+        //    int a = -5;
+        //    Console.WriteLine($"a = {a}");
+        //    int b = 5;
+        //    Console.WriteLine($"b = {b} ");
+        //    int h = 2;
+        //    Console.WriteLine($"h = {h}");
+        //    int x = a;
+        //    int y;
+        //    for (int i = a; i <= b; i++)
+        //    {
+        //        y = (5*x)*(5*x)-2*x+1;
+        //        x = x + h;
+        //        Console.WriteLine($"y={y}");
+        //    }
+
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+
+        ////задан5
+        //static void Func()
+        //{
+        //    Console.WriteLine("Введите x: ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    int y;
+        //    if (x < 5)
+        //    {
+        //        y = (4 * x * 4 * x + 1) / x - 5;
+        //        Console.WriteLine($"y={y }");
+        //    }
+        //    else 
+        //    {
+        //        y = 3 * x * 3 * x - 2;
+        //        Console.WriteLine($"y={y}");
+        //    }
+
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+        ////задан6
+        //public static int Add(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        //public static int Diff(int x, int y)
+        //{
+        //    return x - y;
+        //}
+        //public static int Mult(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //public static int Div(int x, int y)
+        //{
+
+        //    return x / y;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x= ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y= ");
+        //    int y = int.Parse(Console.ReadLine());
+        //    int add = Add(x, y);
+        //    int diff = Diff(x, y);
+        //    int mult = Mult(x, y);
+        //    int div = Div(x, y);
+        //    Console.WriteLine($"x+y={add}\n x-y={diff}\n x*y={mult}\n x/y={div} ");
+        //    Console.ReadKey();
+        //}
+
+
+
+    }
+}
