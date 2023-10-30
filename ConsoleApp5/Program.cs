779e685fafa41e268f5eff39fbe290efdb1e32fe
afonaizer <123896092+afonaizer@users.noreply.github.com>
2023-10-30 08:09:18 +0200

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -163,6 +163,50 @@ namespace ConsoleApp5
         //    Console.ReadKey();
         //}
 
+     //static void Main(string[] args)
+        //{
+
+        домашнее задание
+        //1.
+        //Console.Write("Введите число a: ");
+        //int a = int.Parse(Console.ReadLine());
+        //Console.Write("Введите число b: ");
+        //int b = int.Parse(Console.ReadLine());
+        //if (a > b)
+        //{ Console.WriteLine($"Число a = {a} максимальное"); }
+        //else if (a < b)
+        //{ Console.WriteLine($"Число b = {b} максимальное"); }
+        //else { Console.WriteLine($"Числа a = {a} и b = {b} оба являются макисмальными"); }
+        //Console.ReadKey();
+
+
+        //2.
+        //Console.Write("Введите число a: ");
+        //double a = int.Parse(Console.ReadLine());
+        //Console.Write("Введите число b: ");
+        //double b = int.Parse(Console.ReadLine());
+        //Console.Write("Введите число c: ");
+        //double c = int.Parse(Console.ReadLine());
+        //double average = (a + b + c) / 3;
+        //Console.WriteLine($"Среднее врифметическое = {average:f2}");
+        //Console.ReadKey();
+        //}
+
+
+        //3.
+        static void Func()
+        {
+            for (double x = -9; x <= 9; x += 3)
+            { Console.WriteLine($"x={x}\ty={4*Math.Pow(x, 2) + 5 * x - 10}"); }
+
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+
+
 
 
     }
